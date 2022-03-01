746c5317760535e1de0eeebeca88f3ea2cec5419
Ömer Aran <omeraran@Omer-MacBook-Air.local>
2022-03-01 11:02:00 +0300

    1 March 2022/packages added

---
+++ Test/src/testpackage/Main.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World, it's me .");
+		System.out.println("I will make a web backend application then i'll add frontend.");
+
+	}
+
+}
